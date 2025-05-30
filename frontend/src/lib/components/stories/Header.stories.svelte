a981548009f6d8946b4a4b5afe43508994c03a35
Nafis <nafis.faiyaz.5@gmail.com>
2025-05-30 22:17:52 +0600

    Updated frontend

--- frontend/src/lib/components/stories/Header.stories.svelte
+++ frontend/src/lib/components/stories/Header.stories.svelte
@@ -7,7 +7,7 @@
   const { Story } = defineMeta({
     title: 'Example/Header',
     component: Header,
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    // This Page will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ['autodocs'],
     parameters: {
       // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
--- frontend/src/lib/schemas/app/dashboard.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const formSchema = z.object({
-  url: z.url().min(2).max(50),
-});
-
-export type FormSchema = typeof formSchema;
