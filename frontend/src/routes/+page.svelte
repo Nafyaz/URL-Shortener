00140e6765cf23ba8db677071c418592f4c718f9
Nafis <nafis.faiyaz.5@gmail.com>
2025-06-19 07:15:08 +0600

    frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { type UrlSchema, urlSchema } from "$schemas/url";
   import { zodClient } from "sveltekit-superforms/adapters";
   import { superForm, type SuperValidated, type Infer } from "sveltekit-superforms";
-  import { Field, Control, Label, Description, FieldErrors } from "$lib/components/ui/form";
+  import * as Form from "$lib/components/ui/form";
   import { Input } from "$lib/components/ui/input";
 
   let { data }: { data: { form: SuperValidated<Infer<UrlSchema>> } } = $props();
@@ -14,19 +14,21 @@
   const { form: formData, enhance } = form;
 </script>
 
-<form method="POST" use:enhance>
-  <Field {form} name="url">
-    <Control>
-      {#snippet children({ props })}
-        <Label>URL</Label>
-        <Input {...props} type="url" bind:value={$formData.url} />
-      {/snippet}
-    </Control>
-    <Description>Paste the url you want to shorten</Description>
-    <FieldErrors />
-  </Field>
-</form>
-
+<div class="flex flex-col items-center">
+  <form method="POST" class="w-2/3 space-y-6 text-center" use:enhance>
+    <Form.Field {form} name="url">
+      <Form.Control>
+        {#snippet children({ props })}
+          <Form.Label>URL</Form.Label>
+          <Input {...props} type="url" bind:value={$formData.url} />
+        {/snippet}
+      </Form.Control>
+      <Form.Description>Paste the url you want to shorten</Form.Description>
+      <Form.FieldErrors />
+    </Form.Field>
+    <Form.Button>Shorten</Form.Button>
+  </form>
+</div>
 <!--<aside>-->
 <!--  <p>Form data:</p>-->
 <!--</aside>-->
