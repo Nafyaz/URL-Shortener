64ca1b177893a9ea6fe120a680f26fe520045c1e
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-08-15 19:32:42 +0600

    Updated gitignore files

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,10 +1,23 @@
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { urlSchema } from "$schemas/url";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async () => {
   return {
     form: await superValidate(zod(urlSchema))
   };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, zod(urlSchema));
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    return { form };
+  }
 };
--- frontend/src/routes/bro/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { enhance } from "$app/forms";
-  import type { PageProps } from "./$types";
-  let { form }: PageProps = $props();
-</script>
-
-<form method="POST" use:enhance></form>
